e1f5326e42f28d478b034c02dc38ee934042de01
Brian <unasbrian@gmail.com>
2020-04-18 09:35:33 -0500

    first commit

---
+++ src/test/java/com/app/aforo255account/ServicesBankaforo255ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.aforo255account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesBankaforo255ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
